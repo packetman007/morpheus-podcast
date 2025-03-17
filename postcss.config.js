96d8dd38a61a7c6abb8ac6e6a608d4e758f87da2
Bill Alderson <bill@alderson.us>
2025-03-17 10:26:13 -0500

    Add TypeScript configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
+    },
+    'tailwindcss': {}
+  }
 }
